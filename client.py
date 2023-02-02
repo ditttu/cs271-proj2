c24a2d687083047f5ebcd12204a0aea5931dfa5c
Aditya Gulati <adityagulatiadityagulati@gmail.com>
2023-02-02 11:47:59 -0800

    first commit

---
+++ client.py
@@ -0,0 +1,93 @@
+import socket
+import select
+import sys
+import threading
+import time
+
+import constants
+
+index = int(sys.argv[1])
+port = constants.CLIENT_PORT_PREFIX + index
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+soc.setblocking(False)
+soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+soc_send = []
+for i in range(constants.NUM_CLIENT):
+    temp_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc_send.append(temp_soc)
+
+soc.listen(constants.NUM_CLIENT)
+inputSockets = [soc.fileno(), sys.stdin.fileno()]
+address_list = []
+send_list = []
+run = 1
+snapshot_dict = {}
+current_requests_channel  = {}
+
+
+def handle_input(x, data):
+    global lamport
+    if data:
+        if data[0] == "t":
+            if(int(data[4])>lamport):
+                lamport = int(data[4])
+            lamport = lamport+1
+            message = "reply "+str(lamport)
+            x.send(message.encode())
+            lamport = lamport+1
+        else:
+            if(int(data[5])>lamport):
+                lamport = int(data[5])
+            lamport = lamport+1
+            chain.release([data[4],data[2],data[3]])
+            x.send("reply 0".encode())
+    else:
+        x.close()
+        inputSockets.remove(x)
+
+def snapshot():
+
+def snapshot_initiate(x, data):
+
+def snapshot_continue(x, data):
+
+def token(token_string):
+
+def initiate():
+
+while run:
+    inputready, outputready, exceptready = select.select(inputSockets, [], [])
+
+    for x in inputready:
+        if x == soc.fileno():
+            client, address = soc.accept()
+            address_list.append(address)
+            inputSockets.append(client)
+        elif x == sys.stdin.fileno():
+            request = sys.stdin.readline().split()
+            if request[0] == "exit":
+                run = 0
+            if request[0] == "i":
+                initiate()
+            if request[0] == "ss":
+                thread = threading.Thread(target=snapshot, daemon=True)
+                thread.start()
+            if request[0] == "token":
+                thread = threading.Thread(target=token, arg=(request[1],), daemon=True)
+                thread.start()
+        else:
+            # "ss {#client_num} {#initial_client_num} {#snapshot_id}"
+            # "t {token_string}"
+            data = x.recv(1024).decode().split()
+            #record message all current channels: append to dictionary
+            if data[0] == "ss":
+                if snapshot_dict.has_key((data[2],data[3])) == True:
+                    thread = threading.Thread(target=snapshot_continue, args=(x, data,), daemon=True)
+                    thread.start()
+                else:
+                    thread = threading.Thread(target=snapshot_initiate, args=(x, data,), daemon=True)
+                    thread.start()
+            else:
+
+
+soc.close()
---
+++ constants.py
@@ -0,0 +1,4 @@
+CLIENT_PORT_PREFIX = 6540
+HOST = "localhost"
+NUM_CLIENT = 5
+CONNECTION_GRAPH = [[1], [0, 3], [1], [0, 1, 2, 4], [1, 3]]
