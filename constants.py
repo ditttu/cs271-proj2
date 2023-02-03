3d8e8ae7d2081958d28c0ec96ee5f9c1592b9258
fatihkaleoglu <124305240+fatihkaleoglu@users.noreply.github.com>
2023-02-03 22:57:17 +0000

    Added message delay constant. Also testing the VSCode integration.

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ CLIENT_PORT_PREFIX = 6540
 HOST = "localhost"
 NUM_CLIENT = 5
 CONNECTION_GRAPH = [[1], [0, 3], [1], [0, 1, 2, 4], [1, 3]]
+MESSAGE_DELAY = 3   # seconds of delay when receiving a message
